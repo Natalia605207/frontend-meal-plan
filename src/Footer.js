566333f417898aa0ca44c39454c9a1d4d004a2d0
Natalia Musikhina <149045254+Natalia605207@users.noreply.github.com>
2024-09-29 02:19:01 -0400

    Added my portfolio link

---
+++ src/Footer.js
@@ -0,0 +1,3 @@
+export const Footer = () => {
+    return <p className="my-link">Designed and developed by <a href="https://natalia-webdev.com" target="_blank" rel="noreferrer" className="portfolio">Natalia Musikhina</a></p>
+}
